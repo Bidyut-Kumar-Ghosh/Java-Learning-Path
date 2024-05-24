1fd4c79d847d3efb33dcaebbbae3e2e65392264c
Bidyut <deepghosh369@gmail.com>
2024-05-24 13:22:55 +0530

    Adding How to Get Area of Rectangle

---
+++ Java Variables Examples/CalculateAreaOfRectangle.java
@@ -0,0 +1,17 @@
+public class CalculateAreaOfRectangle {
+    public static void main (String[] args)
+    {
+        int length = 40;
+        int width = 60;
+        int area;
+
+        //Calculate the are of a Rectangle 
+        area = length * width;
+
+        //Print Variables
+        System.out.println("Length is :- " + length);
+        System.out.println("Width is :- "+ width);
+        System.out.println("Area of the Rectangle is  :- "+ area);
+
+    }
+}

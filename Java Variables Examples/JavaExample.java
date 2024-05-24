81b874c94c33d83bc09b9f16d0061372c7e33d47
Bidyut <deepghosh369@gmail.com>
2024-05-24 13:13:31 +0530

    Add Java Examples

---
+++ Java Variables Examples/JavaExample.java
@@ -0,0 +1,16 @@
+public class JavaExample {
+    public static void main(String[] args) {
+        String studentName = "Bidyut Ghosh";
+        int studentID = 67;
+        int studentAge = 22;
+        float studentFee = 72.95f;
+        char studentGrade = 'B';
+
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+    }
+}
